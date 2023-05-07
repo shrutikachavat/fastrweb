e6c5178c786ee98c49173b81fe7cc43153eba153
shrutika <chavatshrutika@gmail.com>
2023-05-08 00:11:47 +0530

    initiated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Kart from './components/Kart';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Kart/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Kart.js
@@ -0,0 +1,11 @@
+
+
+export default function Kart() {
+    return (
+        <>
+            <div className="container">
+                <h3>Kart Check!</h3>
+            </div>
+        </>
+    )
+}
